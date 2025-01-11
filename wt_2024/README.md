09eec4983073b78fccf8b56006cb2ec0cb19b194
V Daita <vdaita@gmail.com>
2025-01-10 19:38:35 -0600

    saving first draft of rewrite

---
+++ wt_2024/README.md
@@ -0,0 +1,10 @@
+# Calculating related keyword using single words and bigrams
+
+## First, clean the compiled corpus text
+```python clean_corpus.py```
+
+## Then, generate a word2vec model based on the cleaned text. 
+
+## Then, generate the keywords (after stopwords removal and everything)
+
+## then, remove the stopwords from the original docs and produce the counts
---
+++ wt_2024/generate_ngram_vectors.py
@@ -0,0 +1,100 @@
+from gensim.utils import tokenize
+import faiss
+import numpy as np
+from tqdm import tqdm
+from gensim.models.keyedvectors import KeyedVectors
+from gensim.models import Word2Vec
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import json
+from rapidfuzz import process
+from fire import Fire
+
+load_dotenv()
+
+def remove_substrings(input_set):
+    sorted_set = sorted(input_set, key=len)
+    result_set = set()
+
+    for string in sorted_set:
+        if not any(string in other for other in result_set):
+            result_set.add(string)
+
+    return list(result_set)
+
+def get_best_ngrams(model_path: str = "ngram_model.model", seed_words_path: str = "seed_words.json", output_path_json: str = "generated_words.json", output_path_txt: str = "generated_words.txt"):
+    model = Word2Vec.load(model_path)
+    model = model.wv
+
+    index = faiss.IndexFlatL2(384)
+    client = OpenAI(
+        api_key=os.environ["OPENAI_API_KEY"]
+    )
+
+
+    ngrams = []
+    ngram_embeddings = []
+
+    # Load in the word_vectors and find the most similar ones
+    for ngram in model.vocab:
+        ngrams.append(ngram)
+        ngram_embeddings.append(model[ngram])
+    index.add(np.array(ngram_embeddings))
+
+
+    seed_words = json.load(open(seed_words_path, 'r'))
+    all_words = {
+        "refined": {},
+        "unrefined": {}
+    } # create 2 options, without refinement, and with refinement
+
+    all_words_txt = "# Generated Words\n\n"
+
+    for category in seed_words:
+        filtered_words = [word for word in seed_words[category] if word in model.key_to_index]
+        vectors = [model[word] for word in filtered_words]
+        average_vector = np.mean(vectors, axis=0)
+        
+        similar_ngrams = index.search(average_vector.reshape(1, -1), 100)
+        similar_ngrams = [ngrams[i] for i in similar_ngrams[1][0]]
+        similar_ngrams = remove_substrings(similar_ngrams)
+
+        all_words["unrefined"][category] = similar_ngrams
+
+        all_words_txt += f"## Unrefined {category}\n\n"
+        all_words_txt += "\n".join(similar_ngrams) + "\n\n"
+        
+        # Refine the words using ChatGPT
+        response = client.chat.completions.create(
+            messages=[{
+                "role": "system",
+                "content": """Select the 150 most relevant keywords that should be included in this. Response JSON format: {"sorted_keywords": ["bigram1", "bigram2", ...]}"""
+            }, {
+                "role": "user",
+                "content": f"# Category: {category} sentiment from an American company \n\n# Bigrams:\n{json.dumps(similar_bigrams)}"
+            }],
+            model="gpt-4o",
+            # temperature=0.0,
+            response_format={"type": "json_object"}
+        )
+        response = response.choices[0].message.content
+        response = json.loads(response)
+        chatgpt_refined_ngrams = response["sorted_keywords"]
+        verified_refined_ngrams = []
+        for bigram in chatgpt_refined_ngrams:
+            if bigram in similar_ngrams:
+                verified_refined_ngrams.append(bigram)
+        all_words["refined"][category] = verified_refined_ngrams
+
+        all_words_txt += f"## Refined {category}\n\n"
+        all_words_txt += "\n".join(verified_refined_ngrams) + "\n\n"
+
+    with open(output_path_json, 'w') as f:
+        json.dump(all_words, f, indent=4)
+    
+    with open(output_path_txt, 'w') as f:
+        f.write(all_words_txt)
+
+if __name__ == "__main__":
+    Fire(get_best_ngrams)
